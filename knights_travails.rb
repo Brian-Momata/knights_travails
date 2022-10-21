6891a9ce284e615580aeff5dc672c6cb6283958e
Brian-Momata <brianmomata@gmail.com>
2022-10-21 09:03:23 +0300

    Add Knight class

--- knights_travails.rb
+++ knights_travails.rb
@@ -3,3 +3,10 @@ class GameBoard
     @board = Array.new(8) { Array.new(8) }
   end
 end
+
+class Knight
+  attr_accessor :up_right, :up_left, :down_right, :down_left, :left_up, :left_down, :right_up, :right_down
+  def initialize(position)
+    @position = position
+  end
+end
